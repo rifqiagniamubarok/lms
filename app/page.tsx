2a6f75c945f32beea47d72958ca1cd61b0b59631
Rifqi Agnia Mubarok <rifqiagniamubarok@gmail.com>
2025-12-18 15:31:58 +0700

    fix: redirect main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
-'use client';
+import { redirect } from 'next/navigation';
 
-import { Button } from '@heroui/react';
-import Link from 'next/link';
-
-export default function Home() {
-  return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center gap-4">
-      <Button as={Link} href="/login" color="primary" className="">
-        Login
-      </Button>
-    </div>
-  );
+export default async function Home() {
+  return redirect('/dashboard');
 }
