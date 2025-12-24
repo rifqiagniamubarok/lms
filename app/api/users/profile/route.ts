ec76f94ab19734724aca25ace20e55eb6ca42609
Rifqi Agnia Mubarok <rifqiagniamubarok@gmail.com>
2025-12-25 06:32:42 +0700

    feat: get profile

--- app/api/users/profile/route.ts
+++ app/api/users/profile/route.ts
@@ -1,71 +1,70 @@
 import { handleAuth } from '@/utils/handleAuth';
 import handleError from '@/utils/handleError';
 import { prisma } from '@/utils/prisma';
+import ResponseError from '@/utils/ResponseError';
 import { NextResponse } from 'next/server';
 import z from 'zod';
-
-const schemaBodyRequest = z.object({
-  page: z.number().min(1).default(1).optional(),
-  limit: z.number().min(1).default(10).optional(),
-});
 
 export async function GET(request: Request) {
   try {
     const decode = await handleAuth(request);
 
-    const { searchParams } = new URL(request.url);
-    const page = parseInt(searchParams.get('page') || '1');
-    const limit = parseInt(searchParams.get('limit') || '10');
-
     // Validate pagination parameters
-    const validatedParams = schemaBodyRequest.parse({ page, limit });
-    const { page: validatedPage = 1, limit: validatedLimit = 10 } = validatedParams;
-    const skip = (validatedPage - 1) * validatedLimit;
 
     const user = await prisma.user.findUnique({
       where: { id: decode.id },
+      include: {
+        level: true,
+      },
     });
 
     if (!user) {
-      return NextResponse.json({
-        success: false,
-        data: 'User not found',
-      });
+      throw new ResponseError(404, 'User not found');
     }
 
-    const [notifications, total] = await Promise.all([
-      prisma.notification.findMany({
-        where: {
-          userId: user.id,
-        },
-        skip,
-        take: validatedLimit,
-        orderBy: {
-          createdAt: 'desc',
-        },
-      }),
-      prisma.notification.count({
-        where: {
-          userId: user.id,
-        },
-      }),
-    ]);
+    const quiz = await prisma.quiz.findMany({
+      where: { classId: user.classId, levelId: user.levelId, status: 'PUBLISHED' },
+      include: {
+        userQuizes: true,
+      },
+    });
 
-    const totalPages = Math.ceil(total / validatedLimit);
+    const quizResponse = quiz.map((q) => {
+      {
+        const userQuiz = q.userQuizes.find((uq) => uq.userId === user.id);
+        return {
+          id: q.id,
+          title: q.title,
+          bestScore: userQuiz ? userQuiz.bestScore : null,
+          currentScore: userQuiz ? userQuiz.currentScore : null,
+          isPassed: userQuiz && userQuiz.currentScore && userQuiz.currentScore >= user.level.kkm ? true : false,
+        };
+      }
+    });
+
+    const response = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      username: user.username,
+      level: {
+        id: user.level.id,
+        name: user.level.name,
+        kkm: user.level.kkm,
+      },
+      quizzes: quizResponse,
+    };
 
     return NextResponse.json({
       success: true,
-      data: notifications,
-      pagination: {
-        page: validatedPage,
-        limit: validatedLimit,
-        total,
-        totalPages,
-        hasNextPage: validatedPage < totalPages,
-        hasPrevPage: validatedPage > 1,
-      },
+      data: response,
     });
   } catch (error) {
     return handleError(error);
   }
 }
+
+const schemaValidation = z.object({
+  name: z.string().optional(),
+  username: z.string().optional(),
+});
